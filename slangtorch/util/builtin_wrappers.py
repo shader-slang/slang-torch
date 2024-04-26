6e7c891680b71dc25c26fada66a4e2dbcff38aa7
Sai Praveen Bangaru <sbangaru@mit.edu>
2024-04-26 16:41:53 -0400

    Add logic for plain vector/matrix inputs to CUDA kernels
    - Adds basic tests for functionality
    - Adds wrapper logic to parse user input into storage types.

--- slangtorch/util/builtin_wrappers.py
+++ slangtorch/util/builtin_wrappers.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+import re
 import torch
 
 DiffTensorView = namedtuple('DiffTensorView', ['value', 'grad'], defaults=[None])
@@ -72,7 +73,73 @@ def make_array_wrapper(module, typename, wrappedTypeMap, makeTypeWrapper):
         
         return tuple, accept_array
 
+
+def make_vector_wrapper(module, typename, wrappedTypeMap, makeTypeWrapper):
+    typeInfoFnName = f"__typeinfo__{typename}"
+    if hasattr(module, typeInfoFnName):
+        typeInfoFn = getattr(module, typeInfoFnName)
+        (fieldnames, fieldtypenames) = typeInfoFn()
+
+        # Vector types get converted into 'VectorStorage' types with an embedded array called "data".
+        # Our strategy here is to use the array wrapper to parse the data and pack it into a singleton tuple.
+        # 
+
+        assert len(fieldnames) == 1
+        assert "data" in fieldnames
+
+        elementType = fieldtypenames[0]
+
+        # Get the wrapper for the element type
+        (_, innerArrayConvertFn) = makeTypeWrapper(module, elementType, wrappedTypeMap)
+
+        def accept_vector(inp):
+            return tuple([innerArrayConvertFn(inp)])
+        
+        wrappedTypeMap[typename] = (tuple, accept_vector)
+        
+        return tuple, accept_vector
+
+
+def make_matrix_wrapper(module, typename, wrappedTypeMap, makeTypeWrapper):
+    typeInfoFnName = f"__typeinfo__{typename}"
+    if hasattr(module, typeInfoFnName):
+        typeInfoFn = getattr(module, typeInfoFnName)
+        (fieldnames, fieldtypenames) = typeInfoFn()
+
+        # Matrix types get converted into 'MatrixStorage' types with an embedded array called "data".
+        # Our strategy here is to use the array wrapper to parse the data and pack it into a singleton tuple.
+        # 
+
+        assert len(fieldnames) == 1
+        assert "data" in fieldnames
+
+        # Parse matrix type name to get the element type and dimensions
+        # Find the first two numbers in the typename of the form 'NxM'
+        m = re.search(r'\d+x\d+', typename)
+        if m is None:
+            raise ValueError(f"Could not parse matrix typename {typename}")
+        dimensions = m.group(0).split('x')
+        assert len(dimensions) == 2
+
+        def accept_matrix(inp):
+            # Check that the input is a tuple of tuples
+            if not isinstance(inp, tuple):
+                raise ValueError(f"Expected tuple, got {type(inp)}")
+            if not all(isinstance(x, tuple) for x in inp):
+                raise ValueError(f"Expected tuple of tuples, got {inp}")
+            if not all(len(x) == int(dimensions[1]) for x in inp):
+                raise ValueError(f"Expected tuple of tuples of length {dimensions[1]}, got {inp}")
+
+            # Flatten the input into a single tuple and nest it in another tuple
+            return (sum(inp, ()),)
+                
+        wrappedTypeMap[typename] = (tuple, accept_matrix)
+        
+        return tuple, accept_matrix
+
 wrappers = {
     'DiffTensorView': make_diff_tensor_view_wrapper,
-    'Array_*': make_array_wrapper
+    'Array_*': make_array_wrapper,
+    '_VectorStorage_*': make_vector_wrapper,
+    '_MatrixStorage_*': make_matrix_wrapper,
 }
